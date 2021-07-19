766dc3377faa3da1d309a56b8b1650d9563b18cd
Matt Hodgman <matthodg1@gmail.com>
2021-07-19 10:08:14 -0600

    corrected method name

--- runFlowSOM.r
+++ runFlowSOM.r
@@ -52,7 +52,7 @@ data_raw <- cbind(Cluster, exprs(data))
 # make cells.csv
 cells <- data_raw[,c('CellID','Cluster')]
 if (as.logical(args[3])) { # inlcude method column
-    Method <- rep(c('FastPG'),nrow(cells))
+    Method <- rep(c('FlowSOM'),nrow(cells))
     cells <- cbind(cells, Method)
 }
 write.table(cells,file=paste(args[4], args[5], sep='/'), row.names=FALSE, quote=FALSE, sep=',') # write data to csv
@@ -62,7 +62,7 @@ write.table(cells,file=paste(args[4], args[5], sep='/'), row.names=FALSE, quote=
 clusterData <- aggregate(subset(data_raw, select=-c(CellID)), list(data_raw[,'Cluster']), mean) # group feature/expression data by cluster and find mean expression for each cluster, remove CellID column
 clusterData <- subset(clusterData, select=-c(Group.1)) # remove group number column because is identical to community assignation number
 if (as.logical(args[3])) { # inlcude method column
-    Method <- rep(c('FastPG'),nrow(clusterData))
+    Method <- rep(c('FlowSOM'),nrow(clusterData))
     clusterData <- cbind(clusterData, Method)
 }
 write.table(clusterData,file=paste(args[4], args[6], sep='/'),row.names=FALSE,quote=FALSE,sep=',') # write data to csv
